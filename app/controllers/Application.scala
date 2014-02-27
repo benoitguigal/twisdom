a9d7f14abec8bb93cf16d0575771a98ae788f51a
benoit <benoit.guigal@gmail.com>
2014-02-27 15:10:27 +0100

    TwitterStreaming + MongoClient

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,29 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
+import play.api.libs.concurrent.Akka
+import jobs.TwitterStreamingActor
+import akka.actor.Props
+import akka.pattern.ask
+import play.api.Play.current
+import akka.util.Timeout
+import java.util.concurrent.TimeUnit
+import play.api.libs.concurrent.Execution.Implicits._
+
 
 object Application extends Controller {
 
+  val streamingActor = Akka.system.actorOf(Props[TwitterStreamingActor], name = "streamingActor")
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def tweets = Action.async {
+    implicit val timeout = Timeout(5, TimeUnit.SECONDS)
+    (streamingActor ? "lastTweet").mapTo[Option[String]].map { lastTweet =>
+      Ok(lastTweet.getOrElse(""))
+    }
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -5,7 +5,9 @@ version := "1.0-SNAPSHOT"
 libraryDependencies ++= Seq(
   jdbc,
   anorm,
-  cache
-)     
+  cache,
+  "org.twitter4j" % "twitter4j-stream" % "3.0.3",
+  "org.reactivemongo" %% "reactivemongo" % "0.10.0"
+)
 
 play.Project.playScalaSettings
